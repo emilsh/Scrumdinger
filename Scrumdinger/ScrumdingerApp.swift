1b3d69ffe85ab17c9445d9cdb13eb445f43f6911
Emil Shafigin <emilshafigin@Emils-MacBook-Pro.local>
2024-01-10 11:17:02 +0600

    First commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Emil Shafigin on 10/1/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
